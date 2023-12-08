e55359b61abbd6147146eec25c63b6f70188be4b
juliaa29sousa <135378912+juliaa29sousa@users.noreply.github.com>
2023-12-08 10:07:01 -0300

    Add files via upload

---
+++ imagens/script.js
@@ -0,0 +1,8 @@
+function openPopup() {
+    document.getElementById('popup').style.display = 'block';
+  }
+  
+  function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+  }
+  
